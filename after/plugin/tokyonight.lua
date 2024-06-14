00d52dd0e49fd59454f7ce78556e336458d205ab
dochallam <dochallam@users.noreply.github.com>
2024-06-14 12:39:10 +0100

    Update tokyonight.lua to make code comments and line numbers clearer
    This had a side-effect - it made some lualine components unreadable due to colour contrast. This was fixed in the previous commit.

--- after/plugin/tokyonight.lua
+++ after/plugin/tokyonight.lua
@@ -22,19 +22,21 @@ require("tokyonight").setup({
   dim_inactive = false, -- dims inactive windows
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
+  -- You can override specific color groups to use other groups or a hex color
+  -- function will be called with a ColorScheme table
+  -- @param colors ColorScheme
   on_colors = function(colors)
-    colors.comment = "#d0d0d0"
-    --colors.fg_gutter = "#a0a0a0"
+    -- Set the colour of comments
+    colors.comment = "#b0b0b0"
+    -- Set the colour of the line numbering
+    colors.fg_gutter = "#808080"
   end,
 
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
+  -- You can override specific highlights to use other groups or a hex color
+  -- function will be called with a Highlights and ColorScheme table
+  --@param highlights Highlights
+  --@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
 })
 
-vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd[[colorscheme tokyonight]]
