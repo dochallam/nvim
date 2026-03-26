37f17c603b449577f1c9160a5f13103fc310fb6c
dochallam <dochallam@users.noreply.github.com>
2026-03-26 16:01:42 +0000

    Update Telescope plugin version from 0.1.5 to 0.1.8

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -72,7 +72,7 @@ require("lazy").setup({
 
     -- Telescope fuzzy finder (https://github.com/nvim-telescope/telescope.nvim)
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
 
